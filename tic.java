9574a143c3e90062e2308858187c80e89ab1a005
Devansh Vashishat <65941949+genius-de@users.noreply.github.com>
2023-10-13 20:10:04 +0530

    fixed check win

--- tic.java
+++ tic.java
@@ -1,1 +1,108 @@
+import java.util.Scanner;
 
+public class ttt {
+    private static char[][] board = new char[3][3];
+    private static char currentPlayer = 'X';
+
+    public static void main(String[] args) {
+        initializeBoard();
+        printBoard();
+
+        boolean gameWon = false;
+
+        while (!gameWon) {
+            int[] move = getPlayerMove();
+            int row = move[0];
+            int col = move[1];
+
+            if (isValidMove(row, col)) {
+                makeMove(row, col);
+                printBoard();
+                gameWon = checkWin(row, col);
+                if (gameWon) {
+                    System.out.println("Player " + currentPlayer + " wins!");
+                } else if (isBoardFull()) {
+                    System.out.println("It's a draw!");
+                    break;
+                } else {
+                    currentPlayer = (currentPlayer == 'X') ? 'O' : 'X';
+                }
+            } else {
+                System.out.println("Invalid move. Please try again.");
+            }
+        }
+    }
+
+    private static void initializeBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = ' ';
+            }
+        }
+    }
+
+    private static void printBoard() {
+        System.out.println("  0 1 2");
+        for (int i = 0; i < 3; i++) {
+            System.out.print(i + " ");
+            for (int j = 0; j < 3; j++) {
+                System.out.print(board[i][j]);
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println();
+            if (i < 2) {
+                System.out.println("  -----");
+            }
+        }
+    }
+
+    private static int[] getPlayerMove() {
+        Scanner scanner = new Scanner(System.in);
+        int[] move = new int[2];
+
+        System.out.print("Player " + currentPlayer + ", enter your move (row and column): ");
+        move[0] = scanner.nextInt();
+        move[1] = scanner.nextInt();
+
+        return move;
+    }
+
+    private static boolean isValidMove(int row, int col) {
+        if (row < 0 || row >= 3 || col < 0 || col >= 3) {
+            return false;
+        }
+        return board[row][col] == ' ';
+    }
+
+    private static void makeMove(int row, int col) {
+        board[row][col] = currentPlayer;
+    }
+
+    private static boolean checkWin(int row, int col) {
+        // Check row, column, and diagonals for a win manually and there is some error in it 
+        //return (board[row][0] == currentPlayer && board[row][1] == currentPlayer && board[row][2] == currentPlayer)
+          //      || (board[0][col] == currentPlayer && board[0][col] == currentPlayer && board[2][col] == currentPlayer)
+            //    || (row == col && board[0][0] == currentPlayer && board[1][1] == currentPlayer && board[2][2] == currentPlayer)
+            //    || (row + col == 2 && board[0][2] == currentPlayer && board[1][1] == currentPlayer && board[2][0] == currentPlayer);
+
+// code from stackoverflow and w3 schools 
+        for (int i = 0; i < 3; i++) {
+            if (board[i][0] == currentPlayer && board[i][1] == currentPlayer && board[i][2] == currentPlayer) {
+                return true;
+            }
+            if (board[0][i] == currentPlayer && board[1][i] == currentPlayer && board[2][i] == currentPlayer) {
+                return true;    }
+
+    private static boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
