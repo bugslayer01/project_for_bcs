14057815f9047f97f8d16c8e20278f77f811d57d
kerito <devansh28112004@gmail.com>
2023-10-15 14:07:56 +0530

    round 2

---
+++ Devansh_bcs_cal/src/main/java/com/mycompany/devansh_bcs_cal/Devansh_bcs_cal.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.devansh_bcs_cal;
+
+/**
+ *
+ * @author kerito
+ */
+public class Devansh_bcs_cal {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
